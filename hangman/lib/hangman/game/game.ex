a6b72fe2e16ea4233a9beb0a4189acdbe866f31e
cogden14 <cogden14@gmail.com>
2018-10-08 14:53:00 -0500

    Add function layout and initial tests

--- hangman/lib/hangman/game/game.ex
+++ hangman/lib/hangman/game/game.ex
@@ -22,16 +22,61 @@ defmodule Hangman.Game do
     end
 
     def tally(game) do   
-        %{
-        t: turns_left,
-        g: game_state,
-        l: letters,
-        u: used,
-        lg: last_guess
+        state = %{
+        game_state: game.status,
+        turns_left: game.turns_left,
+        letters: game.letters,
+        used: game.used
         }
     end
 
     def make_move(game, guess) do
-        :move
+        letters_original = game.letters
+        updated_game = game.letters
+        |> fill_letters(game.word, guess)
+        |> has_changed(letters_original)
+        |> next_state(game.turns_left, game.used, guess)
+        |> get_and_update_state(game)
+    end
+
+    def fill_letters(letters, word, guess) do
+        filler = Enum.map(guess)
+
+        #union must insert only set values from filler, like a right join
+        letters_new = letters
+        |> Enum.join(filler)
+    end
+
+    def has_changed(letters_new, letters_original) do
+        letters_new !==letters_original
+    end
+
+    #:good_guess
+    def next_state(true) do
+    #change status to :good_guess
+        :good_guess
+    end
+
+    #:bad_guess
+    def next_state(false, turns_left, used, last_guess) do 
+    #decrement turns_left, add to used
+    #change status to :bad_guess
+
+    end
+
+    #:already_used
+    def next_state(false) do  
+    #change status to :already_used
+        :already_used
+    end
+
+    def get_and_update_state(true, state) do
+    #check if won
+        state
+    end
+
+    def get_and_update_state(false, state) do
+    #check if lost
+        state
     end
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -2,18 +2,34 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  game = %Hangman.Game.State{
+  defmodule TestStruct do
+  test_game = %Hangman.Game.State{
         turns_left: 7,
         status: :initializing,
-        word: "struct",
+        word: "functional",
         letters: "",
         used: "",
         last_guess: ""
   }
+  end
   test "creates a game" do
     assert Hangman.new_game()
   end
 
   test "test and assert tally" do
-    assert Hangman.tally(game)
+    test_game = %Hangman.Game.State{
+        turns_left: 7,
+        status: :initializing,
+        word: "functional",
+        letters: "",
+        used: "",
+        last_guess: ""
+  }
+    assert Hangman.tally(test_game)
+  end
+
+  test "check has_changed() value" do
+    letters = ["H","e","l","l","o"]
+    assert Hangman.Game.has_changed(letters, Enum.reverse(letters))
+  end
 end
